d55cb944e01142c55a61f71c18d237eb3a222247
RJ <66503358+RJ-Flash@users.noreply.github.com>
2025-03-24 16:50:22 -0400

    Add database session manager

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,10 +1,30 @@
+"""
+Database session management.
+"""
+from typing import Generator
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from app.core.config import settings
+from ..core.config import settings
 
+# Create SQLAlchemy engine
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+
+# Create sessionmaker
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+
+def get_db() -> Generator:
+    """
+    Get a database session.
+    
+    Yields:
+        SQLAlchemy session
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
