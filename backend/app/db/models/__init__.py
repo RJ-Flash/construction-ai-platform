f89c008e2086fa77f009c631a042d207753a4f00
RJ <66503358+RJ-Flash@users.noreply.github.com>
2025-03-24 09:54:05 -0400

    Add db models structure

---
+++ backend/app/db/models/__init__.py
@@ -0,0 +1,7 @@
+from app.db.models.user import User
+from app.db.models.project import Project
+from app.db.models.document import Document
+from app.db.models.element import Element
+from app.db.models.material import Material
+from app.db.models.estimation import Estimation
+from app.db.models.plugin import Plugin
