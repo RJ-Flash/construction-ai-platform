13d4776db02455e91608c1a4ad22d7134eab95b5
RJ <66503358+RJ-Flash@users.noreply.github.com>
2025-03-24 19:27:08 -0400

    Add MEP plugins directory and init file

---
+++ backend/app/plugins/mep/__init__.py
@@ -0,0 +1,4 @@
+# Import plugins to register them
+from .electrical_plugin import ElectricalSystemsPlugin
+from .plumbing_plugin import PlumbingSystemsPlugin
+from .hvac_plugin import HVACSystemsPlugin
